27b2f097d405c5e1958b4ad05e486dacd7b16edc
kay <98snadpragon@gmail.com>
2021-04-25 23:50:55 +0900

    대출 기능

--- bank/account.py
+++ bank/account.py
@@ -1,6 +1,8 @@
 class Account():
     bankAccount = {} #계좌번호, 잔고
     name = {} #계좌번호, 이름
+    total_deposit = 0 # 은행 잔고
+    loan_list = {} # 대출금
 
     #계좌개설
     def makeAccount (self):        
@@ -23,11 +25,13 @@ class Account():
                 accountName = input("이름: ") #이름 정수이면 x
                 if accountName.isdigit() == False:
                     self.name[accountNumber]= accountName
+                    self.loan_list[accountNumber] = 0
                     try:
                         accountMoney = int(input("예금: ")) # 계좌 음수일 수x
                         if accountMoney < 0:
                             raise NotImplementedError()
                         self.bankAccount[accountNumber]= accountMoney
+                        Account.total_deposit = Account.total_deposit + accountMoney * 0.93 #예금의 7%를 제외한 금액 대출 가능
                         print("##계좌개설을 완료하였습니다.##")
                         print("====================")
                     except:
@@ -48,6 +52,7 @@ class Account():
                 if message2 >= 0:
                     self.bankAccount[message1] += message2
                     print("계좌번호: ", message1, "/ 이름: ", self.name[message1],"/ 잔액: ", self.bankAccount[message1])
+                    Account.total_deposit = Account.total_deposit + message2 * 0.93 #예금의 7%를 제외한 금액 대출 가능
                 else:
                     print("금액 입력이 올바르지 않습니다.") # 음수 예외처리
             else:
@@ -83,3 +88,55 @@ class Account():
     
     def endAccount(self):
         print("이용해주셔서 감사합니다!")
+
+    #대출
+    def loan(self):
+        print("======대출하기======")
+        try:
+            checkaccount = int(input("대출받을 계좌번호를 입력해주세요: "))
+            if checkaccount in self.bankAccount.keys():
+                print("계좌이름:", self.name[checkaccount])
+                print("계좌잔고:", self.bankAccount[checkaccount], "원")
+                print("대출 가능 금액:", Account.total_deposit, "원")
+                loan = int(input("대출받을 금액을 입력해주세요: "))
+                if loan > Account.total_deposit:
+                    print("대출 가능 금액 초과")
+                elif loan < 0:
+                    print("금액 입력이 올바르지 않습니다.")
+                else:
+                    self.bankAccount[checkaccount] += loan
+                    Account.total_deposit -= loan
+                    self.loan_list[checkaccount] += loan
+                    print('\n##계좌잔고: ', self.bankAccount[checkaccount], "원 / 대출금: ", self.loan_list[checkaccount], "원##")
+                    print("##대출이 완료되었습니다##")
+                    print("====================")
+            else:
+                print("해당 계좌가 존재하지 않습니다.")
+        except:
+            print("\n===잘못된 계좌번호 입니다.===")
+
+    #대출상환
+    def loan_repayment(self):
+        print("======대출상환======")
+        try:
+            checkaccount = int(input("대출상환할 계좌번호를 입력해주세요: "))
+            if checkaccount in self.bankAccount.keys():
+                print("계좌이름:", self.name[checkaccount])
+                print("계좌잔고:", self.bankAccount[checkaccount], "원")
+                print("대출금: ", self.loan_list[checkaccount], "원")
+                LR = int(input("대출상환할 금액을 입력해주세요: "))
+                if LR > self.loan_list[checkaccount]:
+                    print("대출금 초과")
+                elif LR < 0:
+                    print("금액 입력이 올바르지 않습니다.")
+                else:
+                    self.bankAccount[checkaccount] -= LR
+                    self.loan_list[checkaccount] -= LR
+                    Account.total_deposit += LR
+                    print('\n##계좌잔고: ', self.bankAccount[checkaccount], "원 / 대출금: ", self.loan_list[checkaccount], "원##")
+                    print("##대출상환이 완료되었습니다##")
+                    print("====================")
+            else:
+                print("해당 계좌가 존재하지 않습니다.")
+        except:
+            print("\n===잘못된 계좌번호 입니다.===")
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from bank import Account
+
+#메인
 class Bank(Account):
     def main(self):
         print("\n======Bank Menu=====")
-        print("1. 계좌개설\n2. 입금하기\n3. 출금하기\n4. 전체조회\n5. 종료하기")
+        print("1. 계좌개설\n2. 입금하기\n3. 출금하기\n4. 전체조회\n5. 대출\n6. 대출상환\n7. 종료하기")
         print("====================")
         select =int(input())
         return select
@@ -19,14 +21,12 @@ while True:
         person.pullMoney()
     elif menu == 4:
         person.searchAll()
-    elif menu == 5:
-        person.endAccount() 
+    elif menu == 5: #대출
+        person.loan()
+    elif menu == 6: #대출상환
+        person.loan_repayment()
+    elif menu == 7:
+        person.endAccount()
         break
     else:
         print("잘못 입력하셨습니다.")
-
-
-
-
-
-
